b539b2c8501f9808bee61dbb23c365b57e2c2c0a
Sakibur59 <sakiburrahman5978@gmail.com>
2026-06-03 21:56:36 +0600

    get job post data route added

--- hireloop-server/index.js
+++ hireloop-server/index.js
@@ -35,7 +35,20 @@ async function run() {
 
         const database = client.db("hireloop_db_user");
         const jobCollection = database.collection("jobs");
+        const companyCollection = database.collection("companies");
 
+        app.get('/api/jobs', async(req, res) =>{
+            const query = {};
+            if(req.query.companyId){
+                query.companyId = req.query.companyId;
+            }
+            if(req.query.status){
+                query.status = req.query.status;
+            }
+            const cursor = jobCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
 
         app.post('/api/jobs', async (req, res) => {
             const job = req.body;
