b632f8dccb23c9391fc3276f1550edb2d62b2a21
Sakibur59 <sakiburrahman5978@gmail.com>
2026-06-05 11:08:23 +0600

    my compani get route added

--- hireloop-server/index.js
+++ hireloop-server/index.js
@@ -57,6 +57,16 @@ async function run() {
     });
     // company related apis
 
+    app.get("/api/my/companies", async (req, res) => {
+      const query = {};
+      if (req.query.recruiterId) {
+        query.recruiterId = req.query.recruiterId;
+      }
+      const result = await companyCollection.findOne(query);
+
+      res.send(result || {});
+    });
+
     app.post("/api/companies", async (req, res) => {
       const company = req.body;
       const newCompany = {
