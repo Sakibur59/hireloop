537b39fb9a0b8b3953edf35fa125e957d8ca0c24
Sakibur59 <sakiburrahman5978@gmail.com>
2026-06-06 11:49:21 +0600

    job details get route added

--- hireloop-server/index.js
+++ hireloop-server/index.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -45,7 +45,14 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
-
+    app.get("/api/jobs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {
+        _id: new ObjectId(id),
+      };
+      const result = await jobCollection.findOne(query);
+      res.send(result);
+    });
     app.post("/api/jobs", async (req, res) => {
       const job = req.body;
       const newJob = {
