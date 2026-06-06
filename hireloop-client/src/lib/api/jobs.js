c7984928d66509c46f85aedd8b1d1c588824a6d7
Sakibur59 <sakiburrahman5978@gmail.com>
2026-06-06 11:48:11 +0600

    jobs details page added and also create getjobById api for server fetch

--- hireloop-client/src/lib/api/jobs.js
+++ hireloop-client/src/lib/api/jobs.js
@@ -6,7 +6,9 @@ export const getJobs = async () =>{
     return serverFetch('/api/jobs');
 }
 
-
+export const getJobById = async (jobId) => {
+    return serverFetch(`/api/jobs/${jobId}`);
+}
 
 export const getCompanyJobs = async (companyId, status = 'active') => {
     const res = await fetch(`${baseUrl}/api/jobs?companyId=${companyId}&status=${status}`);
