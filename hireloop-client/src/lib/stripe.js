dc3dc62c8c1a665922496d5a190aaa1dd6dd37d0
Sakibur59 <sakiburrahman5978@gmail.com>
2026-06-08 20:01:05 +0600

    update success page after payment successful

--- hireloop-client/src/lib/stripe.js
+++ hireloop-client/src/lib/stripe.js
@@ -4,3 +4,9 @@ import Stripe from 'stripe'
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
+export const PLAN_PRICE_ID = {
+    'seeker_pro': 'price_1Tg3W2LRdtczoBj21WO0EETn',
+    'seeker_premium': 'price_1Tg3XULRdtczoBj2s6GxcChl',
+    'recruiter_growth': 'price_1Tg3bhLRdtczoBj2XSJ2qYgB',
+    'recruiter_enterprise': 'price_1Tg3aBLRdtczoBj25GTa5DiD'
+}
