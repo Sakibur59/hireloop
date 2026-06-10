50e56c18013f82310bfd58d463597e37fcdcf06b
Sakibur59 <sakiburrahman5978@gmail.com>
2026-06-10 18:45:54 +0600

    company table added in admin dashboard

--- hireloop-client/src/app/auth/signup/page.jsx
+++ hireloop-client/src/app/auth/signup/page.jsx
@@ -128,7 +128,7 @@ export default function SignupPage() {
 
                     {/* Role Selection */}
                     <div className="flex flex-col gap-4">
-                        <Label>Subscription plan</Label>
+                        <Label>Role</Label>
                         <RadioGroup defaultValue="seeker" name="role" onChange = {value => setRole(value)} orientation="horizontal">
                             <Radio value="seeker">
                                 <Radio.Control>
